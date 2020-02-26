d32df5f8c2730639c49688653f4c69a95395df0e
Raul Brindus <raul@xanda.net>
2020-02-26 17:54:33 +0000

    added PostForm method

--- xrest/rest_client.go
+++ xrest/rest_client.go
@@ -6,6 +6,8 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"net/url"
+	"strings"
 )
 
 // Mock structure used for mocking requests
@@ -45,7 +47,7 @@ func AddMock(mock Mock) {
 	mocks[getMockID(mock.HTTPMethod, mock.URL)] = &mock
 }
 
-// Post execute a post request to a given URL
+// Post execute a post request to a given URL with the body
 func Post(url string, body interface{}, headers http.Header) (*http.Response, error) {
 	var jsonBytes []byte
 	var err error
@@ -58,17 +60,37 @@ func Post(url string, body interface{}, headers http.Header) (*http.Response, er
 		return mock.Response, mock.Err
 	}
 
-	// Check if the body is already a string (Eg. JSON string)
+	//// Check if the body is already a string (Eg. JSON string)
 	if w, ok := body.(string); ok {
 		jsonBytes = []byte(w)
 	} else {
+		// Attempt to Marshal into a JSON string
 		jsonBytes, err = json.Marshal(body)
 		if err != nil {
 			return nil, err
 		}
 	}
-
 	request, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(jsonBytes))
+	if err != nil {
+		return nil, err
+	}
+	request.Header = headers
+
+	client := http.Client{}
+	return client.Do(request)
+}
+
+// PostForm issues a POST to the specified URL, with data's keys and values URL-encoded as the request body.
+func PostForm(url string, data url.Values, headers http.Header) (*http.Response, error) {
+	if enabledMocks {
+		mock := mocks[getMockID(http.MethodPost, url)]
+		if mock != nil {
+			return nil, errors.New("no mock found for given request")
+		}
+		return mock.Response, mock.Err
+	}
+
+	request, err := http.NewRequest(http.MethodPost, url, strings.NewReader(data.Encode()))
 	if err != nil {
 		return nil, err
 	}
